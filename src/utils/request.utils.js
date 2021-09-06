c52cf4fb28fd109d9351818d9c854e36498d0cfa
lkelsall <kelsall.lewis@gmail.com>
2021-09-06 15:32:06 +0100

    Fix fetchAddress by adding return to start of promise chain

--- src/utils/request.utils.js
+++ src/utils/request.utils.js
@@ -3,11 +3,12 @@ import axios from "axios";
 const google_api_key = process.env.VUE_APP_GOOGLE_MAPS_KEY;
 
 export const fetchAddress = (coordinates) => {
-  axios
+  return axios
     .get(
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates[0]},${coordinates[1]}&key=${google_api_key}`
     )
     .then((response) => {
+      console.log("request made");
       return response.data.results[0].address_components.slice(2, 4);
     });
 };
