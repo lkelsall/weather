3f2964578dc3a67fedd5dab0859cc4e11b8d5897
lkelsall <kelsall.lewis@gmail.com>
2021-09-07 17:06:29 +0100

    Add chooseIcon call to PageHeader Mounted hook

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,18 +1,29 @@
 <template>
   <header class="page-header">
+    <h3>{{ weatherData.main.temp }}</h3>
     <h2>
-      {{ weatherData.weather[0].description }} {{ weatherData.main.temp }}
+      {{ weatherData.weather[0].description }}
     </h2>
     <h3>{{ placeName }}</h3>
   </header>
 </template>
 
 <script>
+import { chooseIcon } from "../utils/utils.js";
+
 export default {
   name: "PageHeader",
   props: {
     placeName: String,
     weatherData: Object,
+  },
+  mounted() {
+    this.icon = chooseIcon(this.weatherData.weather[0].id);
+  },
+  data() {
+    return {
+      icon: null,
+    };
   },
 };
 </script>
