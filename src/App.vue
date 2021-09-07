5ca39dd2964552a539c32bd322b972e50c5b88ab
lkelsall <kelsall.lewis@gmail.com>
2021-09-07 12:19:58 +0100

    Style SplashScreen

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <template v-if="address">
-      <p>{{ address }}</p>
+      {{ address }}
     </template>
     <template v-else>
       <SplashScreen />
@@ -54,9 +54,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 img {
   height: 100px;
--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="centred-content">
     <img alt="Night hail icon" src="../assets/wi-night-hail.svg" />
     <p>Loading...</p>
   </div>
@@ -10,3 +10,13 @@ export default {
   name: "SplashScreen",
 };
 </script>
+
+<style scoped>
+.centred-content {
+  max-height: 100vh;
+  padding-top: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
